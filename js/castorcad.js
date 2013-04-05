1ea0f859536b09a6c6091e296b38ccf79fc31888
Khalifa Nassik <knassik@gmail.com>
2013-04-05 21:05:33 +0000

    contact.html: remove commented code; add css classes to the form

--- js/castorcad.js
+++ js/castorcad.js
@@ -8,32 +8,32 @@
   $banner.length && $banner.carousel( { interval: 10000 } );
   
   xs.form( document.getElementById( 'contact_form' ), 'contact_form', [
-    { 
+    {
         id   : 'model'
       , name : 'model'
       , type : 'hidden'
       , value: 'user_profile'
     },
-    { 
+    {
         id   : 'id'
       , name : 'uuid'
       , type : 'hidden'
       , value: { type: 'UUID' }
     },
-    { 
+    {
         id       : 'name'
       , name     : 'name'
       , type     : 'text'
       , label    : 'Nom & Prénom'
-      , classes  : { input: 'span4', container: 'control-group' }
+      , style    : { field: 'span4', label: 'control-label', container: 'control-group' }
       , mandatory: true
     },
-    { 
+    {
         id       : 'email'
       , name     : 'email'
       , type     : 'email'
       , label    : 'Email'
-      , classes  : { input: 'span4', container: 'control-group' }
+      , style    : { field: 'span4', label: 'control-label', container: 'control-group' }
       , mandatory: true
     },
     {
@@ -41,18 +41,25 @@
       , name   : 'compagnie'
       , type   : 'text'
       , label  : 'Société'
-      , classes: { input: 'span4', container: 'control-group' }
+      , style: { field: 'span4', label: 'control-label', container: 'control-group' }
     },
-    { 
+    {
         id       : 'message'
       , name     : 'message'
       , type     : 'text_area'
       , label    : 'Message'
       , rows     : 8
-      , classes  : { input: 'input-xlarge span7', container: 'control-group' }
+      , style    : { field: 'input-xlarge span7', label: 'control-label', container: 'control-group demo' }
       , mandatory: true
-    } ] )
-    
+    }
+  ], {
+      submit_label: 'Envoyer'
+    , style : {
+        submit_button: 'btn btn-primary',
+        error        : 'error',
+        success      : 'success'
+      }
+  } )
     .socket_io_server()
   ;
 } ( this );
