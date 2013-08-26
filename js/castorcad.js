08c265011cdbcb0ca752a579d58cdf580dfa2770
Khalifa Nassik <knassik@gmail.com>
2013-08-26 16:42:09 +0100

    use pipelet carousel()

--- js/castorcad.js
+++ js/castorcad.js
@@ -1,5 +1,26 @@
 !function ( exports ) {
-  var $ = jQuery;
+  var $ = jQuery, xs = XS.xs;
+  
+  var images = xs
+    .set( [
+      { src: 'images/16.jpg', title: 'Villa à Geneve'    },
+      { src: 'images/15.jpg', title: 'Villa à Geneve'    },
+      { src: 'images/14.jpg', title: 'Villa à Geneve'    },
+      { src: 'images/17.jpg', title: 'Villa à Geneve'    },
+      { src: 'images/12.jpg', title: 'Lotus Club'        },
+      { src: 'images/13.jpg', title: 'Lotus Club'        },
+      { src: 'images/01.jpg', title: 'Villa à Marrakech' },
+      { src: 'images/03.jpg', title: 'Villa à Marrakech' },
+      { src: 'images/04.jpg', title: 'Villa à Marrakech' },
+      { src: 'images/05.jpg', title: 'Villa à Marrakech' },
+      { src: 'images/06.jpg', title: 'Villa à Marrakech' },
+      { src: 'images/07.jpg', title: 'Villa à Marrakech' },
+      { src: 'images/09.jpg', title: 'Villa à Marrakech' },
+      { src: 'images/11.jpg', title: 'Résidence Deroua'  }
+    ], { auto_increment: true } )
+    .load_images()
+    .carousel( document.getElementById( 'banner' ) )
+  ;
   
   var $banner = $( '#banner'       )
     , $form   = $( '#contact_form' )
