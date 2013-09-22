4e663796f7800255add6c9017d1fd8552b05f082
Jean Vincent <uiteoi@gmail.com>
2013-09-22 22:05:25 +0100

    server.js: requires castorcad.js to enable virual hosting

---
+++ server.js
@@ -0,0 +1,48 @@
+/*  server.js
+    
+    ----
+    
+    Copyright (C) 2013, Connected Sets
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+"use strict";
+
+var XS = require( 'excess' ).XS
+  , xs         = XS.xs
+  , log        = XS.log
+;
+
+require( 'excess/lib/server/http.js' );
+
+/* -------------------------------------------------------------------------------------------
+   de&&ug()
+*/
+var de = true;
+  
+function ug( m ) {
+  log( "castorcad, " + m );
+} // ug()
+
+/* -------------------------------------------------------------------------------------------
+   Start HTTP Servers
+*/
+var servers = xs.set( [
+    { ip_address: '0.0.0.0', port: 80 },
+    // { port: 8043, key: '', cert: '' }, // https server usimg key and cert
+  ], { auto_increment: true } )
+  .http_servers()
+;
+
+require( './castorcad.js' )( servers );
