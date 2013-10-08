4aadcbc2b83f79745e90327d5c57d3d160e26a0b
Jean Vincent <uiteoi@gmail.com>
2013-10-08 15:04:52 +0000

    gallery.js: use unique_set() to preload carousel_images

--- js/gallery.js
+++ js/gallery.js
@@ -1,11 +1,19 @@
 // gallery.js
+!function( exports ) {
 
-XS.xs
-  .socket_io_server()
-  .order( [ { id: 'id' } ] )
+var xs = XS.xs
+  , server = xs.socket_io_server()
+  , carousel_images     = server.flow( 'carousel_images' ).plug( exports.carousel_images.to_uri().unique_set() ).order( [ { id: 'id' } ] )
+  , carousel_thumbnails = server.flow( 'carousel_thumbnails' ).order( [ { id: 'id' } ] )
+;
+
+xs.union( [ carousel_images, carousel_thumbnails] )
+  .trace( 'carousel images and thumnails' )
   .bootstrap_photo_album( document.getElementById( 'photo_matrix' ), document.getElementById( 'banner' ), {
       album          : 'album'
     , images_flow    : 'carousel_images'
     , thumbnails_flow: 'carousel_thumbnails'
   } )
 ;
+
+}( this );
